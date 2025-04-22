c0d674006133723dbf35139b582f5a741d17b4a6
Anu Alabi <anu.alabi@skinvision.com>
2025-04-22 12:49:40 +0200

    add employee data

---
+++ src/app/about-us/employees.ts
@@ -0,0 +1,120 @@
+export const employees = [
+  {
+    employeeFields: {
+      key: "001",
+      parentid: "null",
+      name: null,
+      jobTitle: null,
+      department: "Management",
+      bio: null,
+      picture: null,
+    },
+  },
+  {
+    employeeFields: {
+      key: "011",
+      parentid: "001",
+      name: "Ernest de vris",
+      jobTitle: "Chief Executive Officer",
+      department: "Management",
+      bio: "<p>Experienced business leader at the intersection of technology, energy, and healthcare.</p>\n",
+      picture: {
+        node: {
+          sourceUrl: "https://i.pravatar.cc/250?img=69",
+          altText: "Ernest",
+        },
+      },
+    },
+  },
+  {
+    employeeFields: {
+      key: "012",
+      parentid: "001",
+      name: "Andy Cole",
+      jobTitle: "Chief Operations Officer",
+      department: "Management",
+      bio: "<p>Strategy consultant with over 15 years of experience in Strategy.</p>\n",
+      picture: {
+        node: {
+          sourceUrl: "https://i.pravatar.cc/250?img=52",
+          altText: "Andy Cole",
+        },
+      },
+    },
+  },
+  {
+    employeeFields: {
+      key: "013",
+      parentid: "001",
+      name: "Gina Roland",
+      jobTitle: "Head of Regulatory, Quality & Legal",
+      department: "Management",
+      bio: "<p>Regulatory affairs and legal expert with a focus on medical devices and cosmetics.</p>\n",
+      picture: {
+        node: {
+          sourceUrl: "https://i.pravatar.cc/250?img=5",
+          altText: "Gina Roland",
+        },
+      },
+    },
+  },
+  {
+    employeeFields: {
+      key: "002",
+      parentid: "null",
+      name: null,
+      jobTitle: null,
+      department: "Business & Marketing",
+      bio: null,
+      picture: null,
+    },
+  },
+  {
+    employeeFields: {
+      key: "021",
+      parentid: "002",
+      name: "Tony Stark",
+      jobTitle: "Marketing Manager",
+      department: "Business & Marketing",
+      bio: null,
+      picture: {
+        node: {
+          sourceUrl: "https://i.pravatar.cc/250?img=12",
+          altText: "Tony Stark",
+        },
+      },
+    },
+  },
+  {
+    employeeFields: {
+      key: "022",
+      parentid: "002",
+      name: "Angela Smith",
+      jobTitle: "Business Development Manager",
+      department: "Business & Marketing",
+      bio: null,
+      picture: {
+        node: {
+          sourceUrl: "https://i.pravatar.cc/250?img=10",
+          altText: "Angela Smith",
+        },
+      },
+    },
+  },
+  {
+    employeeFields: {
+      key: "023",
+      parentid: "002",
+      name: "Victoria Hugo",
+      jobTitle: "Commercial & Marketing Manager",
+      department: "Business & Marketing",
+      bio: null,
+      picture: {
+        node: {
+          sourceUrl: "https://i.pravatar.cc/250?img=16",
+          altText: "Victoria Hugo",
+        },
+      },
+    },
+  },
+];
